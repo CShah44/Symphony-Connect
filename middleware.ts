6bf75d76545674ad468451a17e456ceb5c18aafc
Chaitya Shah <chaityashah36@gmail.com>
2024-07-08 18:41:28 +0530

    Added Basic Auth; Yet to implement Forgot Password

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 // todo add public and private routes
 
-export default clerkMiddleware();
+const publicRoutes = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (!publicRoutes(req)) auth().protect();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
